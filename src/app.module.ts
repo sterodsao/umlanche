00e4391991264d88d471ba566806b814644f1269
Rodrigo Weich <rodrigo.weich@ciss.com.br>
2023-10-31 22:34:53 -0300

    implementado configurações de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { EnvModule } from './infra/env/env.module'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './infra/env/env'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    EnvModule,
+  ],
 })
 export class AppModule {}
