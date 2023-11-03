1cbe89d48ed007668e74500c46af0eba89cf6850
Rodrigo Weich <rodrigo.weich@ciss.com.br>
2023-11-02 23:03:04 -0300

    ajustado migrations

--- prisma/migrations/20231102021556_alter_table_prato_alter_column_preco_from_decimal_to_int/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `vl_preco` on the `prato` table. The data in that column could be lost. The data in that column will be cast from `Decimal(15,6)` to `Integer`.
-
-*/
--- AlterTable
-ALTER TABLE "prato" ALTER COLUMN "vl_preco" SET DATA TYPE INTEGER;
--- prisma/migrations/20231103020117_create_table_prato/migration.sql
+++ prisma/migrations/20231103020117_create_table_prato/migration.sql
@@ -2,11 +2,11 @@
 CREATE TABLE "prato" (
     "id_prato" UUID NOT NULL,
     "ds_nome" VARCHAR(100) NOT NULL,
-    "ds_descricao" VARCHAR(200) NOT NULL,
-    "vl_preco" DECIMAL(15,6) NOT NULL,
+    "ds_ingredientes" VARCHAR(200) NOT NULL,
+    "vl_preco" INTEGER NOT NULL,
     "fg_ativo" BOOLEAN NOT NULL DEFAULT true,
     "incluido_em" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "atualizado_em" TIMESTAMP NOT NULL,
+    "alterado_em" TIMESTAMP NOT NULL,
 
     CONSTRAINT "pk_prato" PRIMARY KEY ("id_prato")
 );
